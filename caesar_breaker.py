c21cc71f01a42009d6e24b940ed82597c09d12e2
robert.pentiuc <robert.pentiuc@edu-its.it>
2026-04-14 17:03:09 +0200

    Implementazione ricerca del testo più probabilmente giusto

--- caesar_breaker.py
+++ caesar_breaker.py
@@ -4,6 +4,10 @@ def main():
     for k, testo in brute_force(msg):
         print(f"Messaggio decifrato con chiave {k}: {testo}")
 
+    msg = brute_force(msg)
+    prob_text_key, prob_text = search_prob_text(msg)
+    print(f"\nIl testo con più probabilità è quello con  Chiave : {prob_text_key} , {prob_text}")
+
 
 def brute_force(ciphertext: str) -> list[tuple[int, str]]:
     msg_list = []
@@ -21,6 +25,61 @@ def brute_force(ciphertext: str) -> list[tuple[int, str]]:
         msg_list.append((k, result))
 
     return msg_list
+
+def count_matches(text: str) -> tuple[int, str]:
+    COMMON_ITALIAN = ["di", "il", "che", "è", "e", "la", "a", "per", "in", "un", "ciao"]
+    COMMON_ENGLISH = ["the", "and", "of", "to", "a", "in", "is", "it", "you", "that", "hello"]
+
+    words = text.lower().split()
+    ita_matches = 0
+    eng_matches = 0
+
+    for word in COMMON_ITALIAN:
+        if word in words:
+            ita_matches += 1
+
+    for word in COMMON_ENGLISH:
+        if word in words:
+            eng_matches += 1
+
+    if ita_matches > eng_matches:
+        return ita_matches, "it"
+    else:
+        return eng_matches, "en"
+
+
+
+
+def search_prob_text(msglist: list[tuple[int, str]]) -> tuple[int, str]:
+
+    prob_msg = ""
+    prob_key = 0
+    best_prob_ita = 0
+    best_prob_en = 0
+
+    for k, msg in msglist:
+        prob=count_matches(msg)
+        if prob[1] == "it":
+            if prob[0] > best_prob_ita:
+                best_prob_ita = prob[0]
+                prob_msg_ita = msg
+                prob_key_ita = k
+
+        if prob[1] == "en":
+            if prob[0] > best_prob_en:
+                best_prob_en = prob[0]
+                prob_msg_en = msg
+                prob_key_en = k
+
+
+    if best_prob_ita > best_prob_en:
+        return prob_key_ita, prob_msg_ita
+    else:
+        return prob_key_en, prob_msg_en
+
+
+
+
 
 if __name__ == "__main__":
     main()
